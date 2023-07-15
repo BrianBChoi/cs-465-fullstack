712f741b3e49c07943c8d63ed14d119b0048e518
Brian Choi <choi.brian.97@gmail.com>
2023-07-15 13:05:53 -0700

    Replace meals list with template in Meals page

--- travlr/app_server/controllers/meals.js
+++ travlr/app_server/controllers/meals.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+
+// TODO: Stop loading content by JSON
+var meals_list = JSON.parse(fs.readFileSync('./data/meals.json', 'utf-8'));
+
 /* GET meals view */
 const meals = (req, res) => {
     // For highlighting nav element in header and footer
@@ -7,6 +12,7 @@ const meals = (req, res) => {
         'meals', 
         { 
             title: 'Travlr Getaways',
+            meals_list,
             page: 
             {
                 value: value
