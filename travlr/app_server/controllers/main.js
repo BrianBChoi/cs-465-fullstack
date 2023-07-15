39245352f484ef35641baae170c4ddbea5363f4e
Brian Choi <choi.brian.97@gmail.com>
2023-07-15 11:58:02 -0700

    Replace blog list with template in Home page

--- travlr/app_server/controllers/main.js
+++ travlr/app_server/controllers/main.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+
+// TODO: Stop loading content by JSON
+var home_latest_blogs = JSON.parse(fs.readFileSync('./data/home-latest-blogs.json', 'utf-8'));
+
 /* GET HOMEPAGE */
 const index = (req, res) => {
     // For highlighting nav element in header and footer
@@ -7,6 +12,7 @@ const index = (req, res) => {
         'index', 
         { 
             title: 'Travlr Getaways',
+            home_latest_blogs,
             page: 
             {
                 value: value
