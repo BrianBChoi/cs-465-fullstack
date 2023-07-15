0e92e26f995e2043b589227076271ce9b54e1fcc
Brian Choi <choi.brian.97@gmail.com>
2023-07-15 04:44:13 -0700

    Replace HTML lists with templates in News page

--- travlr/app_server/controllers/news.js
+++ travlr/app_server/controllers/news.js
@@ -1,3 +1,9 @@
+var fs = require('fs');
+
+// TODO: Stop loading content by JSON
+var news_latest_links = JSON.parse(fs.readFileSync('./data/news-latest-links.json', 'utf-8'));
+var news_tips_links = JSON.parse(fs.readFileSync('./data/news-tips-links.json', 'utf-8'));
+
 /* GET news view */
 const news = (req, res) => {
     // For highlighting nav element in header and footer
@@ -7,6 +13,8 @@ const news = (req, res) => {
         'news', 
         { 
             title: 'Travlr Getaways',
+            news_latest_links,
+            news_tips_links,
             page: 
             {
                 value: value
